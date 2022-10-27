f3ccc15b62900babdc7f41e1deca7b2c6a078743
jennaly <jenna@wicot.io>
2022-10-26 17:55:03 -0700

    add challenge

---
+++ 10-27-22.js
@@ -0,0 +1,36 @@
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+
+
+//given a string
+//return a string of "(" and ")"s 
+//for a letter that occurs only once in the string, print "("
+//for a letter that occurs more than once in the string, print ")"
+
+//only letters? any special characters or symbols? => only letters and marks
+//does case matter? a string with "Yy" prints "))"? => case does not matter; "Yy" prints "))"
+//string will never be empty? => yes
+
+//iterate through letters in the string
+//for each letter, check if it appears in more than one index in the string
+//by checking if .indexOf() - a method that returns the index of the FIRST instance/match of the letter - is equal to .lastIndexOf() - a method that returns the index of the LAST instance/match of the letter in the string
+//if .indexOf() === .lastIndexOf(), the letter appears only once in the string, therefore, return "("
+//if .indexOf() !== .lastIndexOf(), the letter appears more than once in the string, therefore, return ")"
+//map values to new array
+//return the new array as a string
+
+function checkDuplicates(string) {
+    return [...string].map(letter => [...string].indexOf(letter) === [...string].lastIndexOf(letter) ? "(" : ")").join("")
+  }
+  
+  console.log(checkDuplicates("din"))      // =>  "((("
+  console.log(checkDuplicates("recede"))   // =>  "()()()"
+  console.log(checkDuplicates("Success"))  // =>  ")())())"
+  console.log(checkDuplicates("(( @"))     // =>  "))((" 
+  
+  
